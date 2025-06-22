ee3bb48e6b3a1fc1ab07f1d5ec09c1f657ae1da6
dupontcam <125046483+dupontcam@users.noreply.github.com>
2025-06-22 17:24:25 -0300

    first commit

---
+++ src/main/java/br/com/dupont/honorarios/honorarios_api/HonorariosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dupont.honorarios.honorarios_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonorariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonorariosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dupont/honorarios/honorarios_api/HonorariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dupont.honorarios.honorarios_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonorariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
